151991d642f5aabb502d83f3493a2acb1bb6c4d4
Extibax <extibax@gmail.com>
2020-01-13 19:42:56 -0500

    practice finished

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
+/* Polyfill */
 import "babel-polyfill";
 
+/* Modules */
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 
+/* Code */
 import Counter from "./Counter";
 import reducer from "./reducers";
 
-const store = createStore(reducer);
+/* Sagas */
+import { helloSaga } from "./sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(helloSaga);
 
 const action = type => store.dispatch({ type });
 
@@ -17,6 +28,7 @@ function render() {
       value={store.getState()}
       onIncrement={() => action("INCREMENT")}
       onDecrement={() => action("DECREMENT")}
+      onIncrementAsync={() => action("INCREMENT_ASYNC")}
     />,
     document.getElementById("root")
   );
